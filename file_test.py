9c0242257ecc961b2b48b847c700ed9206a636b2
welchall <welchall@hotmail.fr>
2026-01-20 23:13:32 +0000

    fix 2 test file

--- file_test.py
+++ file_test.py
@@ -1,26 +1,13 @@
-import pytest
 
 
-def test_calc_addition():
-    output = 2+4
-    assert output == 6
+def test_addition():
+    assert 1 + 1 == 2
 
 
-def test_calc_subtraction():
-    # Fonction test du résultat de 2-4
-    output = 2-4
-    assert output == -2
+def test_subtraction():
+    assert 5 - 3 == 2
 
 
-def test_calc_multiply():
-    # Fonction test du résultat de 2*4
-    output = 2*4
-    assert output == 8
-
-
-def test_string_value():
-    # Fonction test si la résultat renvoie 'hello'
-    output = "hello"
-    assert output == "hello"
-
+def test_multiplication():
+    assert 3 * 4 == 12
 
